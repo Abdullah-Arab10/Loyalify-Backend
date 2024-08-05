33a38d64fd5a3034c676bd2328da7b7cbc8b11a9
wardalbadawy <119790771+ward-albadawy@users.noreply.github.com>
2024-08-05 04:35:32 +0300

    points and some changes

---
+++ Loyalify.Application/Services/PointsServices/Queries/GetUserPoints/GetUserPointsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Loyalify.Domain.Common.Errors;
+using ErrorOr;
+using Loyalify.Application.Common.Interfaces.Persistence;
+using Loyalify.Application.Services.PointsServices.Queries.GetUserPointsp;
+using MediatR;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Loyalify.Application.Services.PointsServices.Queries.GetUserPoints;
+
+public class GetUserPointsQueryHandler(IPointsRepository pointsRepository) : IRequestHandler<GetUserPointsQuery,ErrorOr<GetUserPointsResult>>
+{
+    
+    private readonly IPointsRepository _pointsRepository = pointsRepository;
+
+    public async Task<ErrorOr<GetUserPointsResult>> Handle(GetUserPointsQuery request, CancellationToken cancellationToken) 
+    {
+       
+        var points = await _pointsRepository.GetUserPoints(request.userId);
+    
+        return new GetUserPointsResult((HttpStatusCode)StatusCodes.Status200OK,points);
+    }
+}
---
+++ Loyalify.Contracts/Points/GetUserPointsResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace Loyalify.Contracts.Points;
+
+public class GetUserPointsResponse
+{
+    public HttpStatusCode Status { get; set; }
+    public decimal Points { get; set; } = 0!;
+}
